e2e672b8a489a4956aee255b421797182c81d0a7
Abdul Ahad <aratul859@gmail.com>
2025-06-17 23:54:44 +0600

    Update and rename numbers.py to comment.py

---
+++ comment.py
@@ -0,0 +1,23 @@
+#single line comment(use # or ctrl+/)
+
+#This is a comment
+print("Hello, World!")
+
+
+#Multiline Comments(use # for each line or triple quotes ("""  """))
+
+#example 1 
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#example 2
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
--- numbers.py
+++
@@ -1,1 +0,0 @@
-print('ratul dada crazy')
