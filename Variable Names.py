d480a7ff62c876cd98f5905a93c3d6495065c031
Abdul Ahad <aratul859@gmail.com>
2025-06-18 12:58:55 +0600

    Update Variable Names.py

--- Variable Names.py
+++ Variable Names.py
@@ -17,12 +17,20 @@ myVar = "John"
 MYVAR = "John"
 myvar2 = "John"
 
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
 #Illegal variable names:
 
 2myvar = "John"
 my-var = "John"
 my var = "John"
 
+#This example will produce an error in the result
 
 '''
 #Multi Words Variable Names
@@ -34,10 +42,10 @@ There are several techniques you can use to make them more readable:
 #Camel Case(Each word, except the first, starts with a capital letter:)
 myVariableName = "John"
 
-Pascal Case(Each word starts with a capital letter:)
+#Pascal Case(Each word starts with a capital letter:)
 MyVariableName = "John"
 
-Snake Case(Each word is separated by an underscore character:)
+#Snake Case(Each word is separated by an underscore character:)
 my_variable_name = "John"
 
 
