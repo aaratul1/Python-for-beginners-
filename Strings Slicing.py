cf3121d09048b73a0a728dc06b60b4cbcff1db53
Abdul Ahad <aratul859@gmail.com>
2025-06-19 19:53:20 +0600

    Create Strings Slicing.py
    Signed-off-by: Abdul Ahad  <aratul859@gmail.com>

---
+++ Strings Slicing.py
@@ -0,0 +1,15 @@
+#Slicing
+b = "Hello, World!"
+print(b[2:5])
+
+#slice from the start 
+b = "Hello, World!"
+print(b[:5])
+
+#slice to the end
+b = "Hello, World!"
+print(b[2:])
+
+#Negative Indexing
+b = "Hello, World!"
+print(b[-5:-2])
