9528d0d6bccbb78a13401ba0f9b7fcc2e4b24bd7
Abdul Ahad <aratul859@gmail.com>
2025-06-19 19:37:57 +0600

    Create Strings.py
    Signed-off-by: Abdul Ahad  <aratul859@gmail.com>

---
+++ Strings.py
@@ -0,0 +1,70 @@
+#Strings
+print("Hello")
+print('Hello')
+
+#Quotes Inside Quotes
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#Assign String to a Variable
+a = "Hello"
+print(a)
+
+#Multiline Strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+#Strings are Arrays
+a = "Hello, World!"
+print(a[0])
+print(a[1])
+print(a[2])
+
+#Looping Through a String
+for x in "banana":
+print(x)
+
+#String Length
+a = "Hello, World!"
+print(len(a))
+
+#Check String
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#Use it in an if statement:
+txt = "The best things in life are free!"
+if "free" in txt:
+print("Yes, 'free' is present.")
+
+#Check if NOT
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+#Use it in an if statement:
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+print("No, 'expensive' is NOT present.")
+
+
+
+
+
+
+
+
+
+
+
+
