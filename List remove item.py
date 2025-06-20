d275587983b9ed8cf9dfc1e7bc2ea4e5816374c3
Abdul Ahad <aratul859@gmail.com>
2025-06-20 22:07:30 +0600

    Create List remove item.py
    Signed-off-by: Abdul Ahad  <aratul859@gmail.com>

---
+++ List remove item.py
@@ -0,0 +1,60 @@
+Remove Specified Item
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
+
+If there are more than one item with the specified value, the remove() method removes the first occurrence:
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+
+
+Remove Specified Index
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+
+
+If you do not specify the index, the pop() method removes the last item.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+
+
+The del keyword can also delete the list completely.
+
+  thislist = ["apple", "banana", "cherry"]
+del thislist
+
+
+
+
+The del keyword also removes the specified index:
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+
+
+
+
+
+Clear the List
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+
+
+
