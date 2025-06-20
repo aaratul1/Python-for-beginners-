c1c974f38967d81ed90e1aaf2390cf92e299488d
Abdul Ahad <aratul859@gmail.com>
2025-06-20 16:52:13 +0600

    Update list.py
    Signed-off-by: Abdul Ahad  <aratul859@gmail.com>

--- list.py
+++ list.py
@@ -39,9 +39,26 @@ A list can contain different data types:
 list1 = ["abc", 34, True, 40, "male"]
 print(list1)
 
+#type()
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+The list() Constructor
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
 
 
+Python Collections (Arrays)
+There are four collection data types in the Python programming language:
 
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+
+*Set items are unchangeable, but you can remove and/or add items whenever you like.
+**As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
 
 
 
